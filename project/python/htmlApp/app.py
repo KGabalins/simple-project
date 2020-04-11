96c460390a472e040873d03a3430be59329e8460
kgabalins <raivo.k.g@gmail.com>
2020-04-11 13:58:32 +0300

    editing app.py

--- project/python/htmlApp/app.py
+++ project/python/htmlApp/app.py
@@ -1,13 +1,18 @@
-from flask import Flask
-import os
-
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from KGabalins!"
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
 
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(debug=True,host='0.0.0.0',port=port)
+if __name__ == '__main__':
+   app.run(debug = True)
