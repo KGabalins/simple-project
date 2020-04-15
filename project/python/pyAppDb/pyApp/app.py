3722284ccbd12a7e08f03396824d457943060472
kgabalins <raivo.k.g@gmail.com>
2020-04-15 13:50:20 +0300

    Cleaning old project, and starting better flask project

--- project/python/pyAppDb/pyApp/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.dialects.postgresql import JSON
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres//postgres:db-frknz@db:5432/example?sslmode=disable"
-db = SQLAlchemy(app)
-
-db.drop_all()
