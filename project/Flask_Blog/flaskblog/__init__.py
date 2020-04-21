c42f087668ff87f28c952fa951dc5289a30947fa
kgabalins <raivo.k.g@gmail.com>
2020-04-21 18:02:44 +0300

    Login and register working with database

--- project/Flask_Blog/flaskblog/__init__.py
+++ project/Flask_Blog/flaskblog/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blogSite.db'
 app.config['SECRET_KEY'] = 'fc1fe1d849a56333d60b3b09839d2529'
 # Izveidota datubaazes instance
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
--- project/Flask_Blog/flaskblog/forms.py
+++ project/Flask_Blog/flaskblog/forms.py
@@ -1,7 +1,8 @@
 # Formu klasses izveide
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -17,6 +18,24 @@ class RegistrationForm(FlaskForm):
                             validators=[DataRequired(), EqualTo('password')])
 
     submit = SubmitField('Sign Up')
+
+    # Funkcija kas parbauda vai Username jau eksiste
+    def validate_username(self, username):
+
+        # Query, kas parbauda vai User tabula ir jau Username ar tadu pasu nosaukumu
+        user = User.query.filter_by(username=username.data).first()
+
+        if user:
+            raise ValidationError('This username is already in use!')
+
+    # Funkcija kas parbauda vai Email jau eksiste
+    def validate_email(self, email):
+
+        # Query, kas parbauda vai User tabula Email jau eksiste ar tadu pasu nosaukumu
+        email = User.query.filter_by(email=email.data).first()
+
+        if email:
+            raise ValidationError('This email is already in use!')
 
 class LoginForm(FlaskForm):  
     email = StringField('Email',
