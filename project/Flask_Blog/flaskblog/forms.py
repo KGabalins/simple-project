057b9dabf2da15b38b10d47b3d18745c6b857d09
kgabalins <raivo.k.g@gmail.com>
2020-04-22 11:32:02 +0300

    Adding profile pictures and account update

--- project/Flask_Blog/flaskblog/forms.py
+++ project/Flask_Blog/flaskblog/forms.py
@@ -3,6 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
+from flask_login import current_user
 
 # Reģistrēšanās formas klase
 class RegistrationForm(FlaskForm):
@@ -25,7 +26,6 @@ class RegistrationForm(FlaskForm):
 
         # Query, kas parbauda vai User tabula ir jau Username ar tadu pasu nosaukumu
         user = User.query.filter_by(username=username.data).first()
-
         if user:
             raise ValidationError('This username is already in use!')
 
@@ -33,9 +33,8 @@ class RegistrationForm(FlaskForm):
     def validate_email(self, email):
 
         # Query, kas parbauda vai User tabula Email jau eksiste ar tadu pasu nosaukumu
-        email = User.query.filter_by(email=email.data).first()
-
-        if email:
+        user = User.query.filter_by(email=email.data).first()
+        if user:
             raise ValidationError('This email is already in use!')
 
 # Pieslēgšanās formas klase
@@ -49,3 +48,31 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
 
     submit = SubmitField('Sign In')
+
+# Reģistrēšanās formas klase
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                            validators=[DataRequired(), Length(min=2, max=20)])
+    
+    email = StringField('Email',
+                            validators=[DataRequired(), Email()])
+
+    submit = SubmitField('Update')
+
+    # Funkcija kas parbauda vai Username jau eksiste
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            # Query, kas parbauda vai User tabula ir jau Username ar tadu pasu nosaukumu
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('This username is already in use!')
+
+
+    # Funkcija kas parbauda vai Email jau eksiste
+    def validate_email(self, email):
+
+        if email.data != current_user.email:
+            # Query, kas parbauda vai User tabula Email jau eksiste ar tadu pasu nosaukumu
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('This email is already in use!')
